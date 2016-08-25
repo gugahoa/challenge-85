67da1683bdc238cd33a02905ca4f3905d2a47a30
Gustavo Aguiar <gugahoa@gmail.com>
2016-08-25 12:54:23 -0300

    Better list comprehension for upper function

--- solution.hs
+++ solution.hs
@@ -5,7 +5,7 @@ bottom :: Int -> Int -> Int -> [String]
 bottom width height depth = [replicate width '#' ++ replicate x '+' | x <- replicate (height - depth - 1) depth ++ [depth, (depth - 1)..0]]
 
 upper :: Int -> [String]
-upper depth = [replicate x ' ' ++ replicate 19 ':' ++ '/':[] ++ replicate y '+' | (x, y) <- [(x, y) | x <- [3, 2..1], y <- [0..2], elemIndex x [depth, (depth - 1)..1] == elemIndex y [0..(depth - 1)]]]
+upper depth = [replicate x ' ' ++ replicate 19 ':' ++ '/':[] ++ replicate (depth - x) '+' | x <- [depth, (depth - 1)..1]]
 
 printResult :: [String] -> IO ()
 printResult [] = putStrLn("Empty list")
